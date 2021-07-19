9670efb114e1069a024a4166625fe80dcecc3d4d
OhmniD <dave@ohmnid.com>
2021-07-19 19:13:55 +0100

    fixes flask proxy - response from hibp now passed straight through, including response codes

--- server/controllers/hibp_fetch_controller.py
+++ server/controllers/hibp_fetch_controller.py
@@ -1,25 +1,20 @@
-from flask import Blueprint, Flask
+from flask import Blueprint, Flask, Response
 import sys
 import http.client
+import requests
 from services.hibp_headers import headers
+
 
 hibp_fetch_blueprint = Blueprint("hibp_fetch", __name__)
 
 @hibp_fetch_blueprint.route("/hibp_fetch/<email>", methods=["GET"])
 def hibp_info(email):
-    conn = http.client.HTTPSConnection("haveibeenpwned.com")
-    payload = ''
-    conn.request("GET", f"/api/v3/breachedaccount/{email}/?truncateResponse=false", payload, headers)
-    res = conn.getresponse()
-    print(res.status)
-    data = res.read()
-    return data
+    url = f"https://haveibeenpwned.com/api/v3/breachedaccount/{email}?truncateResponse=false"
+    response = requests.get(url, headers=headers)
+    return Response(response.content, response.status_code, headers)
 
 @hibp_fetch_blueprint.route("/hibp_fetch/no_detail/<email>", methods=["GET"])
 def hibp_info_no_detail(email):
-    conn = http.client.HTTPSConnection("haveibeenpwned.com")
-    payload = ''
-    conn.request("GET", f"/api/v3/breachedaccount/{email}", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    return data
+    url = f"https://haveibeenpwned.com/api/v3/breachedaccount/{email}"
+    response = requests.get(url, headers=headers)
+    return Response(response.content, response.status_code, headers)
